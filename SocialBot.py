154335c0794980d5bd1666496f67276d3c7f0c57
tommy91 <tommaso.vuaran@gmail.com>
2017-05-23 16:17:42 +0200

    error correction

--- SocialBot.py
+++ SocialBot.py
@@ -1520,7 +1520,7 @@ from SBProg import SBProg
 def removeFilesByExtension(ext):
 	filelist = [ f for f in os.listdir(".") if f.endswith(ext) ]
 	for f in filelist:
-    	os.remove(f)
+		os.remove(f)
 
 
 def clean():
@@ -1542,23 +1542,23 @@ def clean():
 
 
 if __name__ == '__main__':
-    lp = len(sys.argv)
-    if lp > 2:
-        print("\n\tError: " + str(lp-1) + " params, admitted only 1! Ignored all.\n")
-    elif lp > 1:
-    	if(sys.argv[1]=='-clean'):
- 			clean()
-        if(sys.argv[1]=='-f'):
-            print("\n\tFast Mode On.\n\tNo sleep char/line.\n")
-            program = SBProg(sleepChar=0.0, sleepLine=0.0)
-        elif(sys.argv[1]=='-t'):
-            print("\n\tTest Mode On.\n")
-            program = SBProg(isTest=True)
-        elif(sys.argv[1] in ['-ft','-tf']):
-            print("\n\tFast Mode On.\n\tNo sleep char/line.\n\n\tTest Mode On.\n")
-            program = SBProg(isTest=True, sleepChar=0.0, sleepLine=0.0)
-        else:
-            print("\n\tError: unknown command '" + sys.argv[1] + "', ignored.\n")
-            program = SBProg()
-    program.runProgram()
+	lp = len(sys.argv)
+	if lp > 2:
+		print("\n\tError: " + str(lp-1) + " params, admitted only 1! Ignored all.\n")
+	elif lp > 1:
+		if(sys.argv[1]=='-clean'):
+			clean()
+		if(sys.argv[1]=='-f'):
+			print("\n\tFast Mode On.\n\tNo sleep char/line.\n")
+			program = SBProg(sleepChar=0.0, sleepLine=0.0)
+		elif(sys.argv[1]=='-t'):
+			print("\n\tTest Mode On.\n")
+			program = SBProg(isTest=True)
+		elif(sys.argv[1] in ['-ft','-tf']):
+			print("\n\tFast Mode On.\n\tNo sleep char/line.\n\n\tTest Mode On.\n")
+			program = SBProg(isTest=True, sleepChar=0.0, sleepLine=0.0)
+		else:
+			print("\n\tError: unknown command '" + sys.argv[1] + "', ignored.\n")
+			program = SBProg()
+	program.runProgram()
 
