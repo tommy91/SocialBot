1fd087fda63dc1bd58ce030f8db22dd7f4a76d94
tommy91 <tommaso.vuaran@gmail.com>
2017-06-30 12:23:36 +0200

    error correction

--- Output.py
+++ Output.py
@@ -3,12 +3,25 @@ import logging
 from Settings import LOGFILE_ERROR
 
 
+formatter = logging.Formatter(fmt='%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%y %H:%M:%S')
+
+
 class Output:
 
 
 	def __init__(self, logpath):
-		self.infoLog = logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.INFO, datefmt='%m/%d/%y %H:%M:%S', filename=logpath)
-		self.errorLog = logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.DEBUG, datefmt='%m/%d/%y %H:%M:%S', filename=LOGFILE_ERROR)
+		self.infoLog = self.setup_logger('infoLog', logpath)
+		self.errorLog = self.setup_logger('errorLog', LOGFILE_ERROR, level=logging.DEBUG)
+
+
+	def setup_logger(name, log_file, level=logging.INFO):
+	    """Function setup as many loggers as you want"""
+	    handler = logging.FileHandler(log_file)        
+	    handler.setFormatter(formatter)
+	    logger = logging.getLogger(name)
+	    logger.setLevel(level)
+	    logger.addHandler(handler)
+	    return logger
 
 
 	def write(self, res):
